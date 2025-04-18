90f42dfa599c7329a40625a40af10cd11e9d3804
Katja Hollaar <afkatja@gmail.com>
2025-04-18 11:30:43 -0600

    add utils for text

---
+++ src/lib/string.ts
@@ -0,0 +1,9 @@
+const truncateString = (str: string, length: number): string => {
+  const trimmedString =
+    str.length > length
+      ? str.substring(0, length - 3) + "..."
+      : str.substring(0, length)
+  return trimmedString
+}
+
+export { truncateString }
