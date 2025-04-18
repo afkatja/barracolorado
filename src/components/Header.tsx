9ddf20f5f19011b61ad464141001b0d0f50198ff
Katja Hollaar <afkatja@gmail.com>
2025-04-18 11:30:57 -0600

    update layout for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,21 +8,23 @@ const Header = async () => {
 
   return (
     <header id="header" className="bg-gray-800">
-      <Link href="/" className="text-3xl text-slate-900 font-black">
-        <Image
-          src="/images/logo.png"
-          alt="Barra del Colorado"
-          width={100}
-          height={100}
-        />
-      </Link>
-      <nav>
-        <ul className="flex space-x-4">
-          {[...mainNav, ...routeNav].map(item => (
-            <NavItem key={item.id} item={item} />
-          ))}
-        </ul>
-      </nav>
+      <div className="mx-auto w-11/12 flex items-center">
+        <Link href="/" className="text-3xl text-slate-900 font-black">
+          <Image
+            src="/images/logo.png"
+            alt="Barra del Colorado"
+            width={100}
+            height={100}
+          />
+        </Link>
+        <nav className="ml-auto">
+          <ul className="flex space-x-4">
+            {[...mainNav, ...routeNav].map(item => (
+              <NavItem key={item.id} item={item} />
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
