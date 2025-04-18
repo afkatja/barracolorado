11378d829db39652d324a22cde7aa28e8dbad149
Katja Hollaar <afkatja@gmail.com>
2025-04-17 20:55:58 -0600

    update navigation for navitems

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -1,38 +1,49 @@
 import { sanityFetch } from "../sanity/lib/client"
-import { NAV_QUERY } from "../sanity/lib/queries"
+import { NAV_QUERY, NAV_ROUTE_QUERY } from "../sanity/lib/queries"
 
-type navItem = {
+export type navItem = {
   _id: string
   title: string
   slug: { current: string }
   isPublished: boolean
+  route?: boolean
 }
 const navigation = async () => {
-  const navItems = [
-    { id: "intro", name: "Intro" },
-    { id: "gallery", name: "Gallery" },
-    { id: "contact", name: "Contact" },
-  ]
+  // const navItems = [
+  //   { id: "intro", name: "Intro" },
+  //   { id: "gallery", name: "Gallery" },
+  //   { id: "contact", name: "Contact" },
+  // ]
+
+  const mapNav = (navItems: navItem[]) =>
+    navItems
+      .map(item => ({
+        href: item?.slug.current,
+        id: item._id,
+        name: item.title,
+        ...item,
+      }))
+      .filter(item => item.isPublished)
+      .sort((a, b) => {
+        if (a.title < b.title) return -1
+        if (a.title > b.title) return 1
+        return 0
+      })
 
   const mainNav = await sanityFetch<navItem[]>({
     query: NAV_QUERY,
     revalidate: 0,
-    params: { category: "Pages" },
+    params: { category: "Pages", route: false },
   })
 
-  const main = mainNav
-    .map(item => ({
-      href: item?.slug.current,
-      id: item._id,
-      name: item.title,
-      ...item,
-    }))
-    .filter(item => item.isPublished)
-    .sort((a, b) => {
-      if (a.title < b.title) return -1
-      if (a.title > b.title) return 1
-      return 0
-    })
-  return { navItems, main }
+  const routeNav = await sanityFetch<navItem[]>({
+    query: NAV_ROUTE_QUERY,
+    revalidate: 0,
+    params: { category: "Pages", route: true },
+  })
+
+  const main = mapNav(mainNav)
+  const routeNavItems = mapNav(routeNav)
+  return { main, routeNavItems }
 }
 export default navigation
