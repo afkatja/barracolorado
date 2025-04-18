7724ed56607279d671ace3614b7ea00eaf915faa
Katja Hollaar <afkatja@gmail.com>
2025-04-18 11:31:20 -0600

    add topics section for pages

---
+++ src/app/(pages)/topics/[slug]/page.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+import { sanityFetch } from "../../../../sanity/lib/client"
+import { PAGE_QUERY } from "../../../../sanity/lib/queries"
+import { SanityDocument } from "next-sanity"
+import { SanityImageObject } from "@sanity/image-url/lib/types/types"
+import Image from "next/image"
+import { urlFor } from "../../../../sanity/lib/image"
+import RichText from "../../../../components/RichText"
+
+const Page = async ({
+  params,
+}: {
+  params: Promise<Record<string, string>>
+}) => {
+  const { slug } = await params
+  const content = await sanityFetch<{
+    _id: string
+    slug: { current: string }
+    title: string
+    subtitle?: string
+    description?: string
+    body: SanityDocument
+    mainImage?: SanityImageObject & { alt: string }
+  }>({
+    query: PAGE_QUERY,
+    revalidate: 0,
+    params: { slug },
+  })
+
+  const image = urlFor(content?.mainImage)
+
+  return (
+    <>
+      <section className="m-auto w-11/12">
+        <header className="major">
+          <h1 className="text-5xl text-center p-5">{content.title}</h1>
+          {content?.subtitle && <p>{content.subtitle}</p>}
+        </header>
+        {content?.mainImage && (
+          <div className="image main">
+            <Image src={image.url()} alt="test" />
+          </div>
+        )}
+        <RichText body={content.body} />
+      </section>
+    </>
+  )
+}
+
+export default Page
