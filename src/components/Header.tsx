a8fe3765a2a99fc76486ccbaa956070cca86e9ce
Katja Hollaar <afkatja@gmail.com>
2025-05-04 14:43:51 -0600

    fix logo and import as a comp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,17 @@
 import Link from "next/link"
 // import navigation from "../lib/navigation"
 // import NavItem from "./NavItem"
-import Image from "next/image"
 import Navigation from "./Navigation"
+import { Logo } from "./icons"
 
 const Header = async () => {
   // const { main: mainNav } = await navigation()
 
   return (
     <header id="header" className="bg-linear-to-bl from-teal-800 to-cyan-900">
-      <div className="mx-auto w-11/12 flex items-center p-1">
-        <Link href="/" className="text-3xl text-slate-900">
-          <Image
-            src="/images/logo.svg"
-            alt="Barra del Colorado"
-            width={70}
-            height={70}
-          />
+      <div className="mx-auto w-11/12 flex items-center py-1">
+        <Link href="/" className="block w-6 h-6 text-3xl text-slate-900">
+          <Logo className="fill-gray-50" />
         </Link>
         <Navigation />
         {/* <nav className="ml-auto">
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,5 @@
 import Close from "./Close"
 import ChevronDown from "./ChevronDown"
+import Logo from "./logo"
 
-export { Close, ChevronDown }
+export { Close, ChevronDown, Logo }
