e1f4917d44acb7b81261ba697622ab50d3229ec8
Katja Hollaar <afkatja@gmail.com>
2025-05-06 21:33:41 -0600

    allow for same slug with different locales

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { SlugValidationContext } from "sanity"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function isUniqueOtherThanLanguage(
+  slug: string,
+  context: SlugValidationContext
+) {
+  const { document, getClient } = context
+  if (!document?.language) {
+    return true
+  }
+  const client = getClient({ apiVersion: "2025-05-06" })
+  const id = document._id.replace(/^drafts\./, "")
+  const params = {
+    draft: `drafts.${id}`,
+    published: id,
+    language: document.language,
+    slug,
+  }
+  const query = `!defined(*[
+    !(_id in [$draft, $published]) &&
+    slug.current == $slug &&
+    language == $language
+  ][0]._id)`
+  const result = await client.fetch(query, params)
+  return result
 }
