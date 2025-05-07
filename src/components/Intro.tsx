2c26858a0d0b828b3a879ad246fd0f34104e0aed
Katja Hollaar <afkatja@gmail.com>
2025-05-06 21:35:22 -0600

    dynamic values for intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,19 +1,31 @@
 "use client"
 import { Link, Element } from "react-scroll"
+import { SanityImageObject } from "@sanity/image-url/lib/types/types"
+import { urlFor } from "../sanity/lib/image"
 
-const Intro = () => (
+const Intro = ({
+  title,
+  subtitle,
+  image,
+  description,
+}: {
+  title: string
+  subtitle: string
+  image: SanityImageObject
+  description: string
+}) => (
   <Element name="intro" className="main fullscreen">
     <section
       id="intro"
-      className="dark style1 bg-gray-900 text-white flex justify-center items-center p-8 flex-1"
+      className="dark style1 bg-gray-900 text-white flex justify-center items-center p-8 flex-1 background-attachment-fixed background-size-cover background-position-center background-repeat-no-repeat"
+      style={{ backgroundImage: `url(${urlFor(image).url()})` }}
     >
       <div className="content text-center">
         <header>
-          <h2 className="text-4xl mb-4">Hola.</h2>
+          <h2 className="text-4xl mb-4 font-black">{title}</h2>
         </header>
-        <p className="mb-4">
-          Welcome to <strong>Barra Coronado</strong>
-        </p>
+        <h3 className="mb-4 font-bold text-xl">{subtitle}</h3>
+        <p className="mb-4">{description}</p>
         <footer className="relative py-4">
           <Link
             to="one"
