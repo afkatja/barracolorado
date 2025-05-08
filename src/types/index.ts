b02720a362148b876d48cb0a96549fa774748853
Katja Hollaar <afkatja@gmail.com>
2025-05-07 20:20:15 -0600

    types

---
+++ src/types/index.ts
@@ -0,0 +1,61 @@
+import { SanityDocument } from "@sanity/client"
+import { SanityImageObject } from "@sanity/image-url/lib/types/types"
+
+export type Locale = "en" | "fr" | "nl" | "es"
+
+export type PageParams = {
+  slug: string
+  lang: Locale
+}
+
+export type PageProps = {
+  params: PageParams
+}
+
+export type Document = {
+  _id: string
+  _type: string
+  title: string
+  language: Locale
+  translations?: SanityDocument[]
+}
+
+export type Page = Omit<Document, "language" | "_type"> & {
+  slug: {
+    current: string
+  }
+  title?: string
+  subtitle?: string
+  description?: string
+  content: string | SanityDocument
+  mainImage: SanityImageObject
+}
+
+export type Post = Document & {
+  slug: {
+    current: string
+  }
+  publishedAt: string
+  excerpt: string
+  coverImage: any // Replace with your image type
+  content: any // Replace with your content type
+}
+
+export type Home = Document & {
+  subtitle: string
+  description: string
+  image: SanityImageObject
+}
+
+export type Gallery = {
+  title: string
+  description?: string
+  images: Array<{
+    _key: string
+    asset: {
+      _ref: string
+    }
+    alt: string
+    caption?: string
+  }>
+}
