485c44f642f2346f40e8f0f51e846ddfd8388d99
Katja Hollaar <afkatja@gmail.com>
2025-05-13 18:03:28 -0600

    fix: only allow one home page per language

--- src/sanity/schemaTypes/homeType.ts
+++ src/sanity/schemaTypes/homeType.ts
@@ -1,5 +1,4 @@
 import { defineField, defineType } from "sanity"
-// import { isUniqueOtherThanLanguage } from "../../lib/utils"
 
 export const homeType = defineType({
   name: "home",
@@ -10,7 +9,25 @@ export const homeType = defineType({
       name: "language",
       type: "string",
       readOnly: true,
-      // hidden: true,
+      validation: rule =>
+        rule.required().custom(async (value, context) => {
+          if (!value || !context.document) return true
+          const { document, getClient } = context
+          const client = getClient({ apiVersion: "2025-05-06" })
+          const id = document._id.replace(/^drafts\./, "")
+          const params = {
+            draft: `drafts.${id}`,
+            published: id,
+            language: value,
+          }
+          const query = `!defined(*[
+          !(_id in [$draft, $published]) &&
+          _type == "home" &&
+          language == $language
+        ][0]._id)`
+          const result = await client.fetch(query, params)
+          return result || "Only one home document is allowed per language"
+        }),
     }),
     defineField({
       name: "title",
@@ -30,4 +47,15 @@ export const homeType = defineType({
       type: "image",
     }),
   ],
+  preview: {
+    select: {
+      title: "title",
+      language: "language",
+    },
+    prepare({ title, language }) {
+      return {
+        title: `${title} (${language})`,
+      }
+    },
+  },
 })
