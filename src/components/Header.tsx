18a98aae2aeecf1e88cba2fb763d95f77fb9176f
Katja Hollaar <afkatja@gmail.com>
2025-05-13 20:16:38 -0600

    chore: minimize lang in lang selector for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,14 @@ const Header = () => {
       className="bg-linear-to-bl from-teal-800 to-cyan-900 z-50 h-[var(--header-height)]"
     >
       <div className="mx-auto w-11/12 flex items-center py-1">
-        <Link href="/" className="block w-6 h-6 text-3xl text-slate-900">
+        <Link
+          href="/"
+          className="block w-6 h-6 text-3xl text-slate-900 order-1"
+        >
           <Logo className="fill-gray-50" />
         </Link>
-        <Navigation />
         <LanguageSelector />
+        <Navigation />
       </div>
     </header>
   )
