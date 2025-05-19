0af4991c69a4f7c8cba555690e91c6e35568fc2b
Katja Hollaar <afkatja@gmail.com>
2025-05-18 21:01:28 -0600

    chore: page card uses card animation

--- src/app/[lang]/(pages)/[slug]/page.tsx
+++ src/app/[lang]/(pages)/[slug]/page.tsx
@@ -46,8 +46,8 @@ const Page = async ({ params }: PageProps) => {
         <div className="main flex-1 md:fullscreen bg-linear-to-br from-teal-800 to-cyan-900 py-2">
           <section className="md:p-4 w-11/12 mx-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 md:gap-4">
-              {pages.map(page => (
-                <PageCard key={page._id} page={page} slug={slug} />
+              {pages.map((page, i) => (
+                <PageCard key={page._id} page={page} slug={slug} index={i} />
               ))}
             </div>
           </section>
--- src/app/[lang]/blog/PostCard.tsx
+++ src/app/[lang]/blog/PostCard.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import BlogCardAnimation from "../../../components/BlogCardAnimation"
+import CardAnimation from "../../../components/CardAnimation"
 import { urlFor } from "../../../sanity/lib/image"
 import Image from "next/image"
 import Link from "next/link"
@@ -8,7 +8,7 @@ import { ArrowRightIcon } from "@radix-ui/react-icons"
 
 const PostCard = ({ post, index }: { post: any; index: number }) => {
   return (
-    <BlogCardAnimation key={post._id} index={index}>
+    <CardAnimation key={post._id} index={index}>
       <Link
         href={`/blog/${post.slug.current}`}
         className="group bg-gray-50 p-4 rounded-lg shadow-md hover:shadow-xl transition-transform duration-300 hover:-translate-y-1 flex flex-col gap-3"
@@ -34,7 +34,7 @@ const PostCard = ({ post, index }: { post: any; index: number }) => {
           <ArrowRightIcon className="ml-2" />
         </div>
       </Link>
-    </BlogCardAnimation>
+    </CardAnimation>
   )
 }
 
