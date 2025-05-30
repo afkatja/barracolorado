b2ca4c34e3a9fec947d0412567a456df56e0863e
Katja Hollaar <afkatja@gmail.com>
2025-05-30 15:26:08 -0600

    chore: display packages on packages page, in the navigation

--- src/app/[lang]/packages/[slug]/Package.tsx
+++ src/app/[lang]/packages/[slug]/Package.tsx
@@ -7,10 +7,12 @@ import Section from "@/components/Section"
 import Dialog from "@/components/Dialog"
 import BookingForm from "@/components/BookingForm"
 import { TFormData } from "@/types"
+import Breadcrumbs from "../../../../components/Breadcrumbs"
 
 const Package = ({
   packageData,
   formData,
+  lang,
 }: {
   packageData: {
     _id: string
@@ -19,13 +21,25 @@ const Package = ({
     subtitle?: string
     description?: string
     mainImage?: SanityImageObject
+    price?: string
+    slug: { current: string }
   }
   formData?: TFormData
+  lang: string
 }) => {
-  const { _id, title, content, subtitle, description, mainImage } = packageData
+  const { _id, title, content, subtitle, description, mainImage, price, slug } =
+    packageData
 
   return (
     <>
+      <div className="w-11/12 mx-auto py-1">
+        <Breadcrumbs
+          items={[
+            { label: "Packages", href: `/${lang}/packages` },
+            { label: title, href: `/packages/${slug.current}` },
+          ]}
+        />
+      </div>
       {title && content && (
         <Section
           key={_id}
@@ -37,11 +51,17 @@ const Package = ({
           image={mainImage}
           asSection={false}
         >
-          {formData && (
-            <Dialog button={{ buttonChildren: "Book now" }}>
-              <BookingForm data={formData} />
-            </Dialog>
-          )}
+          <footer className="flex items-center border-t-1 border-gray-600 pt-2 mt-2">
+            <dl className="flex items-center">
+              <dt className="m-0! text-lg font-bold">Price per person</dt>
+              <dd className="m-0!">{price}</dd>
+            </dl>
+            {formData && (
+              <Dialog button={{ buttonChildren: "Book now" }}>
+                <BookingForm data={formData} orderData={packageData} />
+              </Dialog>
+            )}
+          </footer>
         </Section>
       )}
     </>
