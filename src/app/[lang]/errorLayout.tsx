b4c481e4a747dcf71baed95f9bce48896fa3aef2
Katja Hollaar <afkatja@gmail.com>
2025-05-31 12:00:56 -0600

    fix: error layout

--- src/app/[lang]/errorLayout.tsx
+++ src/app/[lang]/errorLayout.tsx
@@ -17,36 +17,38 @@ const inter = Inter({ subsets: ["latin"] })
 
 const errorLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen ${inter.className}`}
+    // <html lang="en">
+    //   <body
+    //     className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen ${inter.className}`}
+    //   >
+    <>
+      <header
+        id="header"
+        className="bg-linear-to-bl from-teal-800 to-cyan-900 z-50 h-[var(--header-height)]"
       >
-        <header
-          id="header"
-          className="bg-linear-to-bl from-teal-800 to-cyan-900 z-50 h-[var(--header-height)]"
-        >
-          <div className="mx-auto w-11/12 flex items-center py-1">
-            <Link href="/" className="block w-6 h-6 text-3xl text-slate-900">
-              <Logo className="fill-gray-50" />
-            </Link>
+        <div className="mx-auto w-11/12 flex items-center py-1">
+          <Link href="/" className="block w-6 h-6 text-3xl text-slate-900">
+            <Logo className="fill-gray-50" />
+          </Link>
+        </div>
+      </header>
+      <main
+        className="flex flex-col"
+        style={{
+          viewTimelineName: "--scroll-timeline",
+          scrollTimelineName: "--scroll-timeline",
+        }}
+      >
+        <div className="min-h-[calc(100vh-var(--header-height)*1.5)] flex items-center justify-center bg-linear-to-br from-teal-800 to-cyan-900">
+          <div className="bg-gray-100 rounded-lg shadow-lg my-4 p-4 max-w-2xl mx-auto text-center">
+            {children}
           </div>
-        </header>
-        <main
-          className="flex flex-col"
-          style={{
-            viewTimelineName: "--scroll-timeline",
-            scrollTimelineName: "--scroll-timeline",
-          }}
-        >
-          <div className=" flex items-center justify-center bg-linear-to-br from-teal-800 to-cyan-900">
-            <div className="bg-gray-100 rounded-lg shadow-lg my-4 p-4 max-w-2xl mx-auto text-center">
-              {children}
-            </div>
-          </div>
-        </main>
-        <Footer />
-      </body>
-    </html>
+        </div>
+      </main>
+      <Footer />
+    </>
+    //   </body>
+    // </html>
   )
 }
 
