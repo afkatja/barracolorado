50dbe3393b6c73a0ec8e6c44de753ee648951861
Katja Hollaar <afkatja@gmail.com>
2025-05-31 13:57:02 -0600

    chore: install eslint-import-plugin and add a preommit hook

---
+++ v9.eslint.config.mjs
@@ -0,0 +1,41 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      '@typescript-eslint/no-require-imports': 'off',
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index"
+          ],
+          "newlines-between": "always",
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          }
+        }
+      ]
+    },
+    plugins: ["import"],
+  }
+];
+
+export default eslintConfig;
